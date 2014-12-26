5626c37e4bd9e63084917d363de217efe04fe927
Roman <darkromanovich@gmail.com>
2014-12-26 06:07:42 -0500

    test

--- example.php
+++ example.php
@@ -6,13 +6,13 @@ $serial = new phpSerial;
 
 // First we must specify the device. This works on both linux and windows (if
 // your linux serial device is /dev/ttyS0 for COM1, etc)
-$serial->deviceSet("COM1");
+$serial->deviceSet("/dev/ttyACM1");
 
 // Then we need to open it
 $serial->deviceOpen();
 
 // To write into
-$serial->sendMessage("Hello !");
+$serial->sendMessage("at+creg?\n",1);
 
 // Or to read from
 $read = $serial->readPort();
