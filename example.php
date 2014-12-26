59469ce216a21c4febcb582d5732d3373150bea9
Roman <darkromanovich@gmail.com>
2014-12-26 06:20:09 -0500

    test

--- example.php
+++ example.php
@@ -7,24 +7,24 @@ $serial = new phpSerial;
 // First we must specify the device. This works on both linux and windows (if
 // your linux serial device is /dev/ttyS0 for COM1, etc)
 $serial->deviceSet("/dev/ttyACM1");
-
+$serial->confBaudRate(9600);
 // Then we need to open it
 $serial->deviceOpen('w+');
 
 stream_set_timeout($serial->_dHandle, 10);
 
-//$serial->confBaudRate(9600);
+
 
 // To write into
 
-$serial->sendMessage("at\n\r",1);
+$serial->sendMessage("at\n",1);
 
 // Or to read from
 $read = $serial->readPort();
 
 echo $read;
 
-$serial->sendMessage("at+creg?\n\r",1);
+$serial->sendMessage("at+creg?\n",1);
 
 // Or to read from
 $read = $serial->readPort();
